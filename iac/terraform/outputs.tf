236c758e51beb8af9cb35617efb836d2bc6bb99b
Desmond Coacher <dcoacher@outlook.com>
2025-11-27 04:02:45 +0200

    Update outputs.tf

--- iac/terraform/outputs.tf
+++ iac/terraform/outputs.tf
@@ -42,6 +42,6 @@ output "SSH-TO-CONTROL-PLANE" {
 
 output "JOIN-TO-K8s-CLUSTER" {
   description = "Command for Joining Workers to K8s Cluster"
-  value       = "After Deploying K8S Env - Run The Following Command on Controller Plane: cat /home/ubuntu/join-command.sh"
+  value       = "After Deploying K8S Env - Run The Following Command on Control Plane: cat /home/ubuntu/join-command.sh"
 }
 
